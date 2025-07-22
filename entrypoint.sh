80dfca2608d4a271011eef3c7368b05cdff6ad4c
Daniel-Sogbey <sogbeydaniel28@gmail.com>
2025-07-22 23:47:21 +0000

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,12 @@ set -e
 export TOGETHERAI_API_KEY="${INPUT_TOGETHERAI_API_KEY}"
 export  GITHUB_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+curl -X POST https://37298b918e00.ngrok-free.app/init \
+-H "Content-Type:application/json" \
+-d '{
+      "repo":"'$GITHUB_REPOSITORY'",
+      "pr_number":"'"$(jq.pull_request.number "$GITHUB_EVENT_PATH")"'",
+      "api_key":"'$TOGETHERAI_API_KEY'",
+    }'
+
 /app/ai-reviewer
--- webhook-service/cmd/router.go
+++ webhook-service/cmd/router.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func router(chunkChan chan Envelope, llmResponseChan chan Envelope, errorChan chan error, engine any) http.Handler {
 	r := http.NewServeMux()
 
-	r.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("POST /webhook", func(w http.ResponseWriter, r *http.Request) {
 		handleWebhook(w, r, chunkChan, llmResponseChan, errorChan, engine)
+	})
+
+	r.HandleFunc("POST /init", func(w http.ResponseWriter, r *http.Request) {
+		var input struct {
+			Repo     string `json:"repo"`
+			PRNumber string `json:"pr_number"`
+			APIKey   string `json:"api_key"`
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&input)
+		if err != nil {
+			log.Println("ERROR DECODING INIT CREDENTIALS:", err)
+			return
+		}
+
+		//credentials := fmt.Sprintf("%s:%s:%s", input.Repo, input.PRNumber, input.APIKey)
+		//
+		//err = os.Setenv("REQUEST_CREDENTIALS", credentials)
+		//if err != nil {
+		//	log.Println("ERROR SETTING ENV VALUES: ", err)
+		//	return
+		//}
+
+		configInstance.AuthKey = input.APIKey
+
+		log.Println("INPUT CREDENTIALS: ", input)
+
+		w.WriteHeader(http.StatusOK)
 	})
 
 	return r
